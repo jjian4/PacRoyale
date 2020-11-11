c6607af4c6d8ac20a6d5ae47838dad3f7f488f74
hassaanaliw <hassaanaliw@gmail.com>
2020-11-12 01:52:04 +0500

    Added firebase

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -7,4 +7,16 @@ export const PAGES = {
   ARENA: "arena",
   MINIGAME: "minigame",
   ABOUT: "about",
+  SPLASH_SCREEN: "splash_screen",
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAIPYSCzBarY45ObEhm8BQu9jAhnIsUcNE",
+  authDomain: "final-project-f528e.firebaseapp.com",
+  databaseURL: "https://final-project-f528e.firebaseio.com",
+  projectId: "final-project-f528e",
+  storageBucket: "final-project-f528e.appspot.com",
+  messagingSenderId: "312001451038",
+  appId: "1:312001451038:web:72f6416555e32201a7ca0f",
+  measurementId: "G-F0TLFCPVG2"
 };
